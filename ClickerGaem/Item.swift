34753bfd2a473cf1056a58afe0624b7dceea8dfc
Justin Covell <me@justincovell.net>
2024-09-13 20:58:03 -0700

    Initial Commit

---
+++ ClickerGaem/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ClickerGaem
+//
+//  Created by Justin Covell on 9/13/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
